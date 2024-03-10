d939c0a165dd281174a5448f49b1d786c8e5a702
Dhananjay Mahtha <dhananjaymahtha29@gmail.com>
2024-03-10 19:59:55 +0530

    Update db.js

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,3 +1,6 @@
 // Connect to the database using Sequelize
 import { Sequelize } from "sequelize";
-export const sequelize = new Sequelize('postgres://postgresuser:D1q3BLHMHYfrgZhwoGzLMvQc9Pf4u0Gv@dpg-cnmrauuv3ddc73flfbsg-a.singapore-postgres.render.com/taskhub_9evw?ssl=true');
+export const sequelize = new Sequelize({
+    url: process.env.DB_URI,
+    dialect: 'postgres'
+});
